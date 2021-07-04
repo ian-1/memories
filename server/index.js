a80b07857269fecc076b2e4a28de6cac667429d4
ian-1 <iandavidwalton@mail.com>
2021-07-04 12:26:23 +0100

    Set CONNECTION_URL for mongo db

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ inport express from 'express';
 inport bodyParser from 'body-parser';
 inport mongoose from 'mongoose';
 inport cors from 'cors';
+inport config from './config.js'
+
+const app = express();
+
+app.use(bodyParser.json({
+  limit: "30mb",
+  extended: true
+}));
+app.use(bodyParser.urlencoded({
+  limit: "30mb",
+  extended: true
+}));
+app.use(cors());
+
+const CONNECTION_URL = `mongodb+srv://ian:${config.password}@cluster0.ey1iq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
